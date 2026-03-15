3e626e6593c527ae5e1c2a9bcfd02c1c68250807
CatShadow <val.kenya@hotmail.fr>
2026-03-15 22:08:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function favicon(url){
         const domain = new URL(url).hostname
         return `https://www.google.com/s2/favicons?sz=64&domain=${domain}`
     }catch{
-        return "globe.avif"
+        return ""
     }
 }
 
